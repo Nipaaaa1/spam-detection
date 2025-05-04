08524f5b988c1bc8b287fd8245cad7fe610826fd
Nipaaaa1 <frostone224@gmail.com>
2025-05-05 06:33:53 +0700

    feat(models): add naive bayes model

---
+++ models/naive-bayes.ts
@@ -0,0 +1,47 @@
+export class NaiveBayes {
+  private spamWordCounts: Record<string, number> = {};
+  private hamWordCounts: Record<string, number> = {};
+  private spamTotalWords = 0;
+  private hamTotalWords = 0;
+  private spamDocs = 0;
+  private hamDocs = 0;
+  private vocabulary: Set<string> = new Set();
+
+  train(label: "spam" | "ham", tokens: string[]) {
+    if (label === "spam") this.spamDocs++;
+    else this.hamDocs++;
+
+    for (const token of tokens) {
+      this.vocabulary.add(token);
+      if (label === "spam") {
+        this.spamWordCounts[token] = (this.spamWordCounts[token] || 0) + 1;
+        this.spamTotalWords++;
+      } else {
+        this.hamWordCounts[token] = (this.hamWordCounts[token] || 0) + 1;
+        this.hamTotalWords++;
+      }
+    }
+  }
+
+  predict(tokens: string[]): "spam" | "ham" {
+    const vocabSize = this.vocabulary.size;
+    const totalDocs = this.spamDocs + this.hamDocs;
+
+    let spamScore = Math.log(this.spamDocs / totalDocs);
+    let hamScore = Math.log(this.hamDocs / totalDocs);
+
+    for (const token of tokens) {
+      const spamCount = this.spamWordCounts[token] || 0;
+      const hamCount = this.hamWordCounts[token] || 0;
+
+      const pTokenSpam = (spamCount + 1) / (this.spamTotalWords + vocabSize);
+      const pTokenHam = (hamCount + 1) / (this.hamTotalWords + vocabSize);
+
+      spamScore += Math.log(pTokenSpam);
+      hamScore += Math.log(pTokenHam);
+    }
+
+    return spamScore > hamScore ? "spam" : "ham";
+  }
+
+} // TODO: Save and load model to JSON
