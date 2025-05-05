b64369e740d94725860768df6ef7759fd11a8302
Nipaaaa1 <frostone224@gmail.com>
2025-05-05 08:33:40 +0700

    feat(models): add stopword filtering

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,7 +1,9 @@
+import { stopwords } from "./stopwords";
+
 export const preprocess = (text: string): string[] => {
   return text
     .toLowerCase()
     .replace(/[^a-z0-9\s]/g, "")
     .split(/\s+/)
-    .filter(Boolean);
+    .filter((word) => word.length > 0 && !stopwords.has(word));
 };
